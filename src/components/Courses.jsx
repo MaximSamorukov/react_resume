40b873b43d9b56fbcbf0866657d35280c9879f69
Maxim Samorukov <maxim.samorukov@gmail.com>
2022-12-03 20:14:52 +0300

    added cosmic js data test in courses

--- src/components/Courses.jsx
+++ src/components/Courses.jsx
@@ -1,18 +1,37 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { LanguageContext } from "../context";
 import { data } from "../data/data";
 import CourseItem from "./CourseItem";
 import c from "./styles/index.module.scss";
+import Cosmic from 'cosmicjs';
+
 
 const Courses = () => {
+  const [cor, setCourses] = useState([]);
+  const api = Cosmic();
+  const bucket = api.bucket({
+    slug: process.env.REACT_APP_COSMIC_SLUG,
+    read_key: process.env.REACT_APP_COSMIC_READ,
+  });
+  useEffect(() => {
+    bucket.objects
+    .find({
+      type: "courses",
+    })
+    .props("metadata")
+    .then((data) => {
+      setCourses(data.objects?.map((i) => i?.metadata));
+    })
+  }, []);
+  console.log(cor);
   const {
     language: { language },
   } = useContext(LanguageContext);
   const courses = data[language]?.courses;
   return (
     <div className={c.courses_layout}>
-      {courses?.map((course) => (
-        <CourseItem className={c} course={course} />
+      {courses?.map((course, index) => (
+        <CourseItem key={index} className={c} course={course} />
       ))}
     </div>
   );
