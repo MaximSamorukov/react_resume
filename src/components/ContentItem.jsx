039c3d7ba46edea15a78fffd054f154672814e62
maksim.samorukov <maksim.samorukov@sphaera.ru>
2024-06-12 15:06:40 +0300

    change theme

--- src/components/ContentItem.jsx
+++ src/components/ContentItem.jsx
@@ -5,17 +5,23 @@ import { Row, Col, Layout } from "antd";
 import { usePageSize } from "../hooks/hooks";
 import { LanguageContext } from "../context";
 import { HeaderItem } from "./header_item";
-import { titles_en, titles_ru, languages } from "../data/libraries";
+import { titles_en, titles_ru, languages, themes } from "../data/libraries";
 import s from './styles/content_item.module.scss';
 
 export const ContentItem = ({ title, children, style = {} }) => {
   const {
+    theme,
     language: { language },
   } = useContext(LanguageContext);
   const [width] = usePageSize();
   const { Content } = Layout;
   const isPresent = width > 750;
   const titles = language === languages.EN ? titles_en : titles_ru;
+  const isBlackTheme = theme === themes.b;
+  const themeStyle = {
+    [s.itemHeader_blackTheme]: isBlackTheme,
+    [s.itemHeader_whiteTheme]: !isBlackTheme,
+  }
   return (
     <div>
       <Content className={s.content}>
@@ -23,7 +29,7 @@ export const ContentItem = ({ title, children, style = {} }) => {
           {isPresent && <Col span={4} />}
           <Col span={5}>
             <div
-              className={cn("ui middle header", s.itemHeader)}
+              className={cn("ui middle header", s.itemHeader, themeStyle)}
             >
               {titles[title]}
             </div>
--- src/components/Wrapper.jsx
+++ src/components/Wrapper.jsx
@@ -1,12 +1,18 @@
-import React from "react";
-import s from "./styles/wrapper.module.scss";
+import React, { useContext } from "react";
+import cn from "classnames";
 import { Animation } from "./animation/index.jsx";
+import { LanguageContext } from "../context/index.js";
+import s from "./styles/wrapper.module.scss";
+import { themes } from "../data/libraries.js";
 
 const Wrapper = ({ children }) => {
+  const { theme } = useContext(LanguageContext);
+  const isBlackTheme = theme === themes.b;
+
   return (
     <div className={s.wrapper_outer}>
        <Animation argument={{}} />
-      <div className={s.wrapper_inner}>{children}</div>
+      <div className={cn(s.wrapper_inner, { [s.whiteTheme]: !isBlackTheme })}>{children}</div>
     </div>
   );
 };
